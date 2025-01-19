5a3c030f336164cd277e429f9298b9b0b5a12b69
rebelscumm <96218199+rebelscumm@users.noreply.github.com>
2025-01-19 11:14:49 -0600

    accel personality

--- selfdrive/controls/lib/sunnypilot/accel_controller.py
+++ selfdrive/controls/lib/sunnypilot/accel_controller.py
@@ -31,12 +31,15 @@ AccelPersonality = custom.AccelerationPersonality
 # accel personality by @arne182 modified by cgw and kumar
 _DP_CRUISE_MIN_V =       [-1.0, -1.0,  -0.88]
 _DP_CRUISE_MIN_V_ECO =   [-1.0, -1.0,  -0.76]
-_DP_CRUISE_MIN_V_SPORT = [-5, -2.8,  -2.0]
+_DP_CRUISE_MIN_V_SPORT = [-5.0, -2.8,  -2.0]
 _DP_CRUISE_MIN_BP =      [0.,   11.1,  20.]
 
-_DP_CRUISE_MAX_V =       [2.0, 2.0, 2.0, 1.75, 1.03, .72,  .53,  .42,  .13]
-_DP_CRUISE_MAX_V_ECO =   [2.0, 2.0, 2.0, 1.50, 0.92, .54,  .43,  .32,  .088]
-_DP_CRUISE_MAX_V_SPORT = [32, 32, 10.0, 5.5, 4.75, 3.25, 3.0, 2.8, 2.6]
+_DP_CRUISE_MAX_V =      [32.0, 32.0, 32.0,32.0,32.0,32.0,32.0, 2.8, 2.6]3]
+_DP_CRUISE_MAX_V_ECO =   [32.0, 32.0, 32.0,32.0,32.0,32.0,32.0, 2.8, 2.6]
+_DP_CRUISE_MAX_V_SPORT = [32.0, 32.0, 32.0,32.0,32.0,32.0,32.0, 2.8, 2.6]
+# _DP_CRUISE_MAX_V =       [2.0, 2.0, 2.0, 1.75, 1.03, .72,  .53,  .42,  .13]
+# _DP_CRUISE_MAX_V_ECO =   [2.0, 2.0, 2.0, 1.50, 0.92, .54,  .43,  .32,  .088]
+# _DP_CRUISE_MAX_V_SPORT = [32, 32, 10.0, 5.5, 4.75, 3.25, 3.0, 2.8, 2.6]
 
 _DP_CRUISE_MAX_BP =      [0.,  1.,  6.,  8.,   11.,  20.,  25.,  30.,  55.]
 
